7b536bfaf4375b18f9e71aca591e242d34bff893
curran <curran.kelleher@gmail.com>
2015-09-02 16:10:51 -0700

    Add tests

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # chiasm-component
+
 A common base for Chiasm plugins.
--- test.js
+++ test.js
@@ -1,12 +1,26 @@
-var assert = require("assert");
+var expect = require('chai').expect
 var ChiasmComponent = require("./index");
 
 describe("ChiasmComponent", function() {
+
   it("should set public properties specified in constructor", function () {
     var component = ChiasmComponent({
       x: 5,
       y: 10
     });
-    assert.equal(component.publicProperties.length, 2);
+    expect(component.publicProperties.length).to.equal(2);
+    expect(component.publicProperties).to.contain("x");
+    expect(component.publicProperties).to.contain("y");
+    expect(component.x).to.equal(5);
+    expect(component.y).to.equal(10);
   });
+
+  it("should set public properties specified via addPublicProperty", function () {
+    var component = ChiasmComponent();
+    component.addPublicProperty("foo", "bar");
+    expect(component.publicProperties.length).to.equal(1);
+    expect(component.publicProperties).to.contain("foo");
+    expect(component.foo).to.equal("bar");
+  });
+
 });
